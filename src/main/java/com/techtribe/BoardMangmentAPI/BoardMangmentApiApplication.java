6fb70fba40c42536dc3279037e12b60b0b46fe01
Aisha1495 <aishaalmamari6@gmail.com>
2023-08-01 12:29:17 +0400

    BoardAPI

---
+++ src/main/java/com/techtribe/BoardMangmentAPI/BoardMangmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.techtribe.BoardMangmentAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardMangmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardMangmentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techtribe/BoardMangmentAPI/BoardMangmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtribe.BoardMangmentAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardMangmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
